d2b1348a93a9fb790f362c8c5ea2e54206bb2225
peralzega <peralta_zega@live.com>
2015-05-08 17:21:58 +0700

    2st commit

--- member/check-book.php
+++ member/check-book.php
@@ -6,10 +6,12 @@ $durasi	= $_POST['durasi'];
 $date	= $_POST['date'];
 $field	= $_POST['field'];
 $total	= $i+$durasi;
-$select = "SELECT * FROM transaksi WHERE PlayDate='$date' AND Field='$field'";
+$select = "SELECT * FROM transaksi WHERE PlayStart>$i AND PlayStart<$total AND PlayDate='$date' AND Field='$field'";
 $query	= mysqli_query($con,$select);
-$fetch	= mysqli_fetch_array($query);
-while ($fetch['PlayStart'] > $total) {
-	header("Location : pay");
-
+$rows	= mysqli_num_rows($query);
+if ($rows > 0) {
+	echo "Kurangi durasi permainan";
+} else {
+	echo "MANTAP";
+}
 ?>
--- test-cek_jam.php
+++ test-cek_jam.php
@@ -1,9 +1,7 @@
 <?php
 include "connect.php";
-$select = "SELECT * FROM transaksi WHERE PlayDate='2015-05-05' AND Field='Semen'";
+$select = "SELECT * FROM transaksi WHERE PlayStart>13 AND PlayDate='2015-05-25' AND Field='Semen'";
 $query	= mysqli_query($con,$select);
 $fetch	= mysqli_fetch_array($query);
-while ($fetch['PlayStart'] >= 13) {
-	echo "ADA"; break;
-} echo "Tidak Ada";
+echo $fetch['PlayStart'];
 ?>
